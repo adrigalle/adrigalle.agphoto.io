2263e480f8a2b1da21142129ef1b64d8bf30c138
adrigalle <adriana.go1105@gmail.com>
2022-10-08 23:43:41 -0700

    routed pages and added more styling

---
+++ index.js
@@ -0,0 +1,34 @@
+var routes = {
+    '':          'home.html',     
+    '/':         'home.html',
+    '#/home':    'home.html',                        
+    '#/about':    'about.html',
+    '#/gallery': 'gallery.html',
+    '#/faqs': 'faqs.html',
+    '#/contact': 'contact.html',
+    '#/services': 'services.html',
+};
+
+async function router(){
+    console.log(location.hash);
+
+    // get requested page
+    var link = window.location.hash;
+
+    // get path (route) for page
+    var route = routes[link];
+
+    // if route exists, load page
+    if (route) loadPage(route);
+};
+router();
+
+async function loadPage(url){
+    // load page
+    const res     = await fetch(url);
+    const content = await res.text();
+    const element = document.getElementById('content');
+    element.innerHTML = content;
+}
+
+window.addEventListener('hashchange', router);
